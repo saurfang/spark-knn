765d86a1ff9e37cb2c1de365eb9029509c03ccfa
Forest Fang <forest.fang@outlook.com>
2015-09-11 00:25:23 -0400

    first try on constructing KNNRDD

--- spark-knn-core/src/main/scala/org/apache/spark/mllib/knn/KNN.scala
+++ spark-knn-core/src/main/scala/org/apache/spark/mllib/knn/KNN.scala
@@ -1,16 +1,51 @@
 package org.apache.spark.mllib.knn
 
-import org.apache.spark.Logging
+import org.apache.spark.{Partitioner, Logging}
 import org.apache.spark.mllib.linalg.Vector
-import org.apache.spark.rdd.RDD
+import org.apache.spark.rdd.{ShuffledRDD, RDD}
+
+import scala.annotation.tailrec
 
 
 class KNN private(val topTreeSize: Int,
                   val topTreeLeafSize: Int
                    ) extends Serializable with Logging {
-  def run(data: RDD[Vector]): KNNRDD = {
+  def run[T](data: RDD[(Vector, T)]): KNNRDD[T] = {
     val sampled = data.sample(false, topTreeSize / data.count()).collect()
     val topTree = MetricTree.create(sampled, topTreeLeafSize)
-    ???
+    val part = new KNNPartitioner(topTree)
+    val repartitioned = new ShuffledRDD[Vector, T, T](data, part)
+    val trees = repartitioned.mapPartitions{
+      itr =>
+        val childTree = MetricTree.create(itr.toIndexedSeq)
+        Iterator(childTree)
+    }
+    new KNNRDD[T](topTree, trees)
+  }
+}
+
+class KNNPartitioner[T](tree: Tree[T]) extends Partitioner {
+  override def numPartitions: Int = Tree.getLeafCount(tree)
+
+  override def getPartition(key: Any): Int = {
+    key match {
+      case v: Vector => searchIndex(new VectorWithNorm(v))
+      case _ => throw new IllegalArgumentException(s"Key must be of type Vector but got: $key")
+    }
+  }
+
+  @tailrec
+  private[this] def searchIndex(v: VectorWithNorm, tree: Tree[T] = tree, acc: Int = 0): Int = {
+    tree match {
+      case node: MetricNode[T] =>
+        val leftDistance = node.leftPivot.fastSquaredDistance(v)
+        val rightDistance = node.leftPivot.fastSquaredDistance(v)
+        if(leftDistance < rightDistance) {
+          searchIndex(v, node.leftChild, acc)
+        } else {
+          searchIndex(v, node.rightChild, acc + Tree.getLeafCount(node.leftChild))
+        }
+      case _ => acc
+    }
   }
 }
--- spark-knn-core/src/main/scala/org/apache/spark/mllib/knn/KNNRDD.scala
+++ spark-knn-core/src/main/scala/org/apache/spark/mllib/knn/KNNRDD.scala
@@ -5,12 +5,12 @@ import org.apache.spark.mllib.linalg.Vector
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{Partition, TaskContext}
 
-class KNNRDD(val rootTree: Tree,
-             @transient childrenTree: RDD[Tree]) extends RDD[Vector](childrenTree) {
+class KNNRDD[T](val rootTree: Tree[T],
+                @transient childrenTree: RDD[Tree[T]]) extends RDD[(Vector, T)](childrenTree) {
 
   @DeveloperApi
-  override def compute(split: Partition, context: TaskContext): Iterator[Vector] =
-    firstParent[Tree].iterator(split, context).flatMap(_.iterator)
+  override def compute(split: Partition, context: TaskContext): Iterator[(Vector, T)] =
+    firstParent[Tree[T]].iterator(split, context).flatMap(_.iterator)
 
   override protected def getPartitions: Array[Partition] = firstParent.partitions
 
