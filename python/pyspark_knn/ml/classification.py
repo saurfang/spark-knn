01e1a331001defd9c29a282ec1aa5e1d3526a73b
zommerfelds <aero_super@yahoo.com>
2016-06-10 17:38:10 +0200

    added Python interface for ml.classification

---
+++ python/pyspark_knn/ml/classification.py
@@ -0,0 +1,62 @@
+from pyspark.ml.wrapper import JavaEstimator, JavaModel
+from pyspark.ml.param.shared import *
+from pyspark.mllib.common import inherit_doc
+from pyspark.ml.util import keyword_only
+
+
+@inherit_doc
+class KNNClassifier(JavaEstimator, HasFeaturesCol, HasLabelCol, HasPredictionCol,
+                    HasProbabilityCol, HasRawPredictionCol, HasInputCols,
+                    HasThresholds, HasSeed, HasWeightCol):
+    @keyword_only
+    def __init__(self, featuresCol="features", labelCol="label", predictionCol="prediction",
+                 seed=None, topTreeSize=1000, topTreeLeafSize=10, subTreeLeafSize=30, bufferSize=-1.0,
+                 bufferSizeSampleSize=list(range(100, 1000 + 1, 100)), balanceThreshold=0.7,
+                 k=5, neighborsCol="neighbors", maxNeighbors=float("inf")):
+        super(KNNClassifier, self).__init__()
+        self._java_obj = self._new_java_obj(
+            "org.apache.spark.ml.classification.KNNClassifier", self.uid)
+
+        self.topTreeSize = Param(self, "topTreeSize", "todo")
+        self.topTreeLeafSize = Param(self, "topTreeLeafSize", "todo")
+        self.subTreeLeafSize = Param(self, "subTreeLeafSize", "todo")
+        self.bufferSize = Param(self, "bufferSize", "todo")
+        self.bufferSizeSampleSize = Param(self, "bufferSizeSampleSize", "todo")
+        self.balanceThreshold = Param(self, "balanceThreshold", "todo")
+        self.k = Param(self, "k", "todo")
+        self.neighborsCol = Param(self, "neighborsCol", "todo")
+        self.maxNeighbors = Param(self, "maxNeighbors", "todo")
+
+        self._setDefault(topTreeSize=1000, topTreeLeafSize=10, subTreeLeafSize=30, bufferSize=-1.0,
+                         bufferSizeSampleSize=list(range(100, 1000 + 1, 100)), balanceThreshold=0.7,
+                         k=5, neighborsCol="neighbors", maxNeighbors=float("inf"))
+
+        kwargs = self.__init__._input_kwargs
+        self.setParams(**kwargs)
+
+    @keyword_only
+    def setParams(self, featuresCol="features", labelCol="label", predictionCol="prediction",
+                  seed=None, topTreeSize=1000, topTreeLeafSize=10, subTreeLeafSize=30, bufferSize=-1.0,
+                  bufferSizeSampleSize=list(range(100, 1000 + 1, 100)), balanceThreshold=0.7,
+                  k=5, neighborsCol="neighbors", maxNeighbors=float("inf")):
+        kwargs = self.setParams._input_kwargs
+        return self._set(**kwargs)
+
+    def _create_model(self, java_model):
+        return KNNClassificationModel(java_model)
+
+
+class KNNClassificationModel(JavaModel):
+    """
+    Model fitted by KNNClassifier.
+    """
+    def __init__(self, java_model):
+        super(KNNClassificationModel, self).__init__(java_model)
+
+        # note: look at https://issues.apache.org/jira/browse/SPARK-10931 in the future
+        self.bufferSize = Param(self, "bufferSize", "todo")
+        self.k = Param(self, "k", "todo")
+        self.neighborsCol = Param(self, "neighborsCol", "todo")
+        self.maxNeighbors = Param(self, "maxNeighbors", "todo")
+
+        self._transfer_params_from_java()
---
+++ python/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="pyspark_knn",
+    url="https://github.com/saurfang/spark-knn",
+    version="0.1",
+    zip_safe=True,
+    packages=find_packages(),
+)
